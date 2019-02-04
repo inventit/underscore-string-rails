6a699d637668d352d37f7ecf2fa335bf87f704ab
Toshiyuki HINA <thina@yourinventit.com>
2019-02-04 17:54:41 +0900

    initial commit

---
+++ lib/underscore/string/rails.rb
@@ -0,0 +1,10 @@
+require "underscore/string/rails/version"
+
+module Underscore
+  module String
+    module Rails
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/underscore/string/rails/version.rb
@@ -0,0 +1,7 @@
+module Underscore
+  module String
+    module Rails
+      VERSION = "0.1.0"
+    end
+  end
+end
